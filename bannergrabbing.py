f4d9a956dd76c1253aada38bb13378a348256722
Rafael Góes <113617142+indiozerah@users.noreply.github.com>
2023-04-11 21:06:09 -0300

    Add files via upload

---
+++ bannergrabbing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import socket
+
+ip = input("Digite o IP: ")
+porta = int(input("Digite a Porta: "))
+
+meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+meusocket.connect((ip,porta))
+banner = meusocket.recv(1024)
+print(banner)
+
